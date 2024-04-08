122b4446744d1eb524b15e214c7487b8bc417733
Raviteja Tanikella <91713140+TVR28@users.noreply.github.com>
2024-04-07 21:15:33 -0700

    Create HelloWorldAppApp.swift

---
+++ HW1/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by User2 on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
